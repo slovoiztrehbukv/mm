4c8d8726002ef4ba1633092a6003505693cd2339
Gleb Andreev <contact@slovoiztrehbukv.ru>
2022-09-07 21:32:19 +0300

    tlg ouath home redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -77,7 +78,18 @@ class LoginController extends Controller
         
         try {
             $data = checkTelegramAuthorization($GET);
-            Log::info("Telegram callback response success", $data);
+            
+            $user = User::firstOrCreate([
+                'tlg_id' => $data['id']
+            ]);
+
+            if (Auth::loginUsingId($user->id)) {
+                request()->session()->regenerate();
+
+                return redirect('/');
+            } else {
+                throw new \Exception("auth atempt failed", $user->toArray());
+            }
         } catch(\Throwable $e) {
             Log::waring("Telegram callback response error: {$e->getMessage()} ", $GET);
         }
